d6d56f1a8b0c4fdf2c2c31a53b4239046591e6b0
Lingxi <lingxi@qt314.ca>
2025-11-18 23:16:10 -0700

    Add Google Analytics tracking (G-7XTCYRXVKD)

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,6 +106,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7XTCYRXVKD"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7XTCYRXVKD');
+            `,
+          }}
+        />
+      </head>
       <body className={`font-sans ${inter.variable} ${jetbrainsMono.variable}`}>
         <BackgroundOrbs />
         <Suspense fallback={null}>{children}</Suspense>
